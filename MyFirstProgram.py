c43c2cd9d2c7cf08b14aa9a6d69e97d87368e630
Myra <W0235012@campus.nscc.ca>
2018-09-10 16:14:06 -0300

    FirstSave

---
+++ MyFirstProgram.py
@@ -0,0 +1,8 @@
+#print("Hello World!")
+#print ("Second Line")
+
+NumberOfStudents=9 #x Value
+NumberOfComputers=3 #y Value   
+NumberOfStudentsWithoutComputers=NumberOfStudents-NumberOfComputers 
+print(NumberOfStudentsWithoutComputers)
+
