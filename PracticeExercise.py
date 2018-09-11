d097d7cf8ce28b5cc97379658054e3d4f61a76a9
AaronMyra <w0235012@campus.nscc.ca>
2018-09-10 22:13:28 -0300

    PracticeExercise_1

---
+++ PracticeExercise.py
@@ -0,0 +1,33 @@
+#Stock Purchase
+costPerShare=25.625
+numberOfShares=400
+markdown=costPerShare*numberOfShares
+print(markdown)
+
+#Break-Even Point
+FixedCost=5000
+PricePerUnit=8
+CostPerUnit=6
+BreakEvenPoint=FixedCost/(PricePerUnit-CostPerUnit)
+print(BreakEvenPoint)
+
+#Saving Account
+Balance=100
+Balance=Balance*1.05+100
+Balance=Balance*1.05+100
+Balance=Balance*1.05
+Balance=round(Balance,2)
+print(Balance)
+
+#Profit From Stock
+PurchasePrice=10
+SellingPrice=15
+PercentProfit=100*(SellingPrice-PurchasePrice)
+print(PercentProfit)
+
+#Projectile MOtion
+InitialHeight=5
+InitialVelocity=50#FPS
+#Height=InitialHeight*InitialVelocity
+Height=-16*3e2+50*3+5
+print(Height)
